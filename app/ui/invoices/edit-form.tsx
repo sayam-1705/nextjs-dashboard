ff95e5fc7285f3d9a6149cec4e1d7c2da3deb129
sayam-1705 <sayam2022mondal@gmail.com>
2025-02-10 21:03:18 +0530

    Fixing bugs related to unused values

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -11,6 +11,7 @@ import Link from "next/link";
 import { Button } from "@/app/ui/button";
 import { updateInvoice, State } from "@/app/lib/actions";
 import { useActionState } from "react";
+import { log } from "console";
 
 export default function EditInvoiceForm({
   invoice,
@@ -22,6 +23,8 @@ export default function EditInvoiceForm({
   const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+
+  console.log("state", state);
 
   return (
     // <form action={updateInvoiceWithId}>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -19,6 +19,8 @@ export default function LoginForm() {
     undefined
   );
 
+  console.log("isPending", isPending);
+
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
